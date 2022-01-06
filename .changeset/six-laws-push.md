a4b0aae23c23c67a4eec8bb4e930d2d75a09338b
Jérémie Parker <hi@pkr.sh>
2022-01-06 17:51:56 +0100

    feat(type): open ended types
    types are more composable
    the geocoder response may contain extra data provided by the original source while preserving a subset of common properties

---
+++ .changeset/six-laws-push.md
@@ -0,0 +1,8 @@
+---
+'@p-j/geocodejson-types': major
+---
+
+open ended types
+
+types are more composable
+the geocoder response may contain extra data provided by the original source while preserving a subset of common properties
--- packages/geocodejson-types/index.d.ts
+++ packages/geocodejson-types/index.d.ts
@@ -1,4 +1,4 @@
-import type { Feature, FeatureCollection, Point } from 'geojson'
+import type { Feature, FeatureCollection, GeoJsonProperties, Point } from 'geojson'
 
 export as namespace GeocodeJSON
 
@@ -16,10 +16,8 @@ export interface GeocodeOptions {
 /**
  * GeocodeJSON Response interface
  */
-export interface GeocodeResponse extends FeatureCollection {
-  // REQUIRED. GeocodeJSON result is a FeatureCollection.
-  type: 'FeatureCollection'
-
+export interface GeocodeResponse<P = GeoJsonProperties, G = JSONObject>
+  extends FeatureCollection<GeocodeFeature<P, G>['geometry'], GeocodeFeature<P, G>['properties']> {
   // REQUIRED. Namespace.
   geocoding: {
     // REQUIRED. A semver.org compliant version number. Describes the version of
@@ -37,70 +35,62 @@ export interface GeocodeResponse extends FeatureCollection {
     // OPTIONAL. Default: null. The query that has been issued to trigger the search.
     query: string | null
   }
-
-  // REQUIRED. As per GeoJSON spec.
-  features: GeocodeResult[]
 }
 
-interface GeocodeResult extends Feature {
-  // REQUIRED. As per GeoJSON spec.
-  properties: {
-    // REQUIRED. Namespace.
-    geocoding: {
-      // REQUIRED. One of "house", "street", "locality", "city", "region", "country" or else...
-      type: GeocodeType
+export type GeocodeFeature<P = GeoJsonProperties, G = JSONObject> = Feature<Point, GeocodeFeatureProperties<G> & P>
 
-      // OPTIONAL. Result accuracy, in meters.
-      accuracy?: number
+export interface GeocodeFeatureProperties<G> extends JSONObject {
+  // REQUIRED. Namespace.
+  geocoding: GeocodingProperties & G
+}
 
-      // RECOMMENDED. Suggested label for the result.
-      label?: string
+export interface GeocodingProperties extends JSONObject {
+  // REQUIRED. One of "house", "street", "locality", "city", "region", "country" or else...
+  type: GeocodeType
 
-      // OPTIONAL. Name of the place.
-      name?: string
+  // OPTIONAL. Result accuracy, in meters.
+  accuracy?: number
 
-      // OPTIONAL. Housenumber of the place.
-      housenumber?: string
+  // RECOMMENDED. Suggested label for the result.
+  label?: string
 
-      // OPTIONAL. Street of the place.
-      street?: string
+  // OPTIONAL. Name of the place.
+  name?: string
 
-      // OPTIONAL. Locality of the place.
-      locality?: string
+  // OPTIONAL. Housenumber of the place.
+  housenumber?: string
 
-      // OPTIONAL. Postcode of the place.
-      postcode?: string
+  // OPTIONAL. Street of the place.
+  street?: string
 
-      // OPTIONAL. City of the place.
-      city?: string
+  // OPTIONAL. Locality of the place.
+  locality?: string
 
-      // OPTIONAL. District of the place.
-      district?: string
+  // OPTIONAL. Postcode of the place.
+  postcode?: string
 
-      // OPTIONAL. County of the place.
-      county?: string
+  // OPTIONAL. City of the place.
+  city?: string
 
-      // OPTIONAL. State of the place.
-      state?: string
+  // OPTIONAL. District of the place.
+  district?: string
 
-      // OPTIONAL. Country of the place.
-      country?: string
+  // OPTIONAL. County of the place.
+  county?: string
 
-      // OPTIONAL. Administratives boundaries the feature is included in,
-      // as defined in http://wiki.osm.org/wiki/Key:admin_level#admin_level
-      // TODO is there some still generic but less OSMish scheme?
-      admin?: Record<string, string>
+  // OPTIONAL. State of the place.
+  state?: string
 
-      // OPTIONAL. Geohash encoding of coordinates (see http://geohash.org/site/tips.html).
-      geohash?: string
-    }
-  }
+  // OPTIONAL. Country of the place.
+  country?: string
 
-  // REQUIRED. As per GeoJSON spec.
-  type: 'Feature'
+  // OPTIONAL. Administratives boundaries the feature is included in,
+  // as defined in http://wiki.osm.org/wiki/Key:admin_level#admin_level
+  // TODO is there some still generic but less OSMish scheme?
+  admin?: Record<string, string>
 
-  // REQUIRED. As per GeoJSON spec.
-  geometry: Point
+  // OPTIONAL. Geohash encoding of coordinates (see http://geohash.org/site/tips.html).
+  geohash?: string
 }
 
 /**
